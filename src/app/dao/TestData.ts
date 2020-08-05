44ad97786102499a32903555215a796f20183599
Error4ik <fearerror@yandex.ru>
2020-08-05 21:36:15 +0300

    Changed numberOfWords in the category.

--- src/app/dao/TestData.ts
+++ src/app/dao/TestData.ts
@@ -4,12 +4,12 @@ import {Card} from '../domain/Card';
 export class TestData {
 
   static categories: Category[] = [
-    {id: '1', title: 'Существительные', numberOfWords: 5},
+    {id: '1', title: 'Существительные', numberOfWords: 3},
     {id: '2', title: 'Прилагательные', numberOfWords: 3},
-    {id: '3', title: 'Неправильные глаголы', numberOfWords: 1},
-    {id: '4', title: 'Правильные глаголы', numberOfWords: 6},
-    {id: '5', title: 'Фразовые глаголы', numberOfWords: 9},
-    {id: '6', title: 'Разговорные фразы', numberOfWords: 14}
+    {id: '3', title: 'Неправильные глаголы', numberOfWords: 5},
+    {id: '4', title: 'Правильные глаголы', numberOfWords: 1},
+    {id: '5', title: 'Фразовые глаголы', numberOfWords: 2},
+    {id: '6', title: 'Разговорные фразы', numberOfWords: 2}
   ];
 
   static cards: Card[] = [
