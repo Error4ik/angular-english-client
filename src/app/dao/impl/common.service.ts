3bc1bc822824a5b1905da3fef5b822da18232ca6
Error4ik <fearerror@yandex.ru>
2020-08-18 22:28:59 +0300

    Added common service.

---
+++ src/app/dao/impl/common.service.ts
@@ -0,0 +1,31 @@
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+export class CommonService<T> {
+
+  private readonly url: string;
+
+  constructor(url: string, protected httpClient: HttpClient) {
+    this.url = url;
+  }
+
+  add(item: T): Observable<T> {
+    return this.httpClient.post<T>(this.url + '/add', item);
+  }
+
+  delete(id: string): Observable<T> {
+    return this.httpClient.delete<T>(this.url + '/delete/' + id);
+  }
+
+  get(id: string): Observable<T> {
+    return this.httpClient.get<T>(this.url + '/id/' + id);
+  }
+
+  update(value: T): Observable<T> {
+    return this.httpClient.post<T>(this.url + '/update', value);
+  }
+
+  findAll(): Observable<T[]> {
+    return this.httpClient.get<T[]>(this.url + '/categories');
+  }
+}
